e104e2be7c8ca0623c4d6ef6a113c2bc6fb61d53
spiritphyz <tonyle@ocf.berkeley.edu>
2016-08-25 10:16:47 -0700

    add sample asserts and app code

---
+++ test/src/app.sample.js
@@ -0,0 +1,10 @@
+var reverseString = function(str) {
+  // your code here
+};
+
+var factorialize = function(num) {
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  return num * factorialize(num - 1);
+};
